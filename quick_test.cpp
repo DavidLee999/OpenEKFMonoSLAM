e44cd4cbbf3179363cdd7abcc226cab450aa74f9
DavidLee999 <MoonLee003@outlook.com>
2017-05-25 11:00:29 +0200

    test file

---
+++ quick_test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <typeinfo>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n = 9;
+	
+	double d = reinterpret_cast<double>(n);
+	
+	cout<<d<<endl<<reinterpret_cast<int>(d)<<endl;
+
+	return 0;
+}
